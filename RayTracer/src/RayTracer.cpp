44e4b7e1854a68102e6ab62c707706ecdab150f4
Lyon77 <rjl7799@gmail.com>
2020-05-16 13:51:15 -0400

    Added color and vec3 helper classes

--- RayTracer/src/RayTracer.cpp
+++ RayTracer/src/RayTracer.cpp
@@ -1,20 +1,28 @@
-// RayTracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "color.h"
+#include "vec3.h"
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int image_width = 256;
+	const int image_height = 256;
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j) 
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i)
+		{
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0.25;
+
+			color pixelColor(r, g, b);
+
+			writeColor(std::cout, pixelColor);
+		}
+	}
+	std::cerr << "\nDone.\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
